5ddb41882cb4324c9701719fc97bf4e4553ff65c
Matthew Clase <mclase@purdue.edu>
2016-04-21 00:38:13 +0000

    new js

--- public/host.js
+++ public/host.js
@@ -2,7 +2,7 @@ var d = req.body;
 var room = getRoom();
 
 function getRoom() {
-	var a = "djafklj";
-	d.getElementById("code").innerHTML = a;
+	var a = "Room Number: djafklj";
+	d.getElementById("RoomNum").innerHTML = a;
 	return a;
 }
