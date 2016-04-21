b983ea19e62e1aef0a421b2b65610bfaeaf55679
Matthew Clase <mclase@purdue.edu>
2016-04-21 02:54:33 +0000

    server?

--- public/host.js
+++ public/host.js
@@ -1,26 +1,10 @@
-<<<<<<< HEAD
-/*eslint-env browser */
-window.console.lo
 
-=======
 /*eslint-env browser, jquery*/
 console.log("running js");
->>>>>>> 556948b6a252f03ba2ea80b7a13fa8b67e799d5f
+var url = "resistancegame.mybluemix.net";
 
-<<<<<<< HEAD
-function getRoom() {
-	
-	
-=======
 window.onload = function getRoom() {
->>>>>>> 556948b6a252f03ba2ea80b7a13fa8b67e799d5f
-	var a = "Room Number: djafklj";
-	console.log("getRoom");
-<<<<<<< HEAD
+
+	var a = jQuery.get(url+"/host");
 	document.getElementById("RoomNum").innerHTML = a;
-	return a;
-=======
-	console.log(a);
-	document.getElementById("RoomNum").innerHTML = a;
->>>>>>> 556948b6a252f03ba2ea80b7a13fa8b67e799d5f
-}
+};
