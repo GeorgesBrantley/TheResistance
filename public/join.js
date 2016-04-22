5de4b63214221f67f6dce84b801c0188c5271b76
Matthew Clase <mclase@purdue.edu>
2016-04-22 19:04:46 +0000

    updated join

--- public/join.js
+++ public/join.js
@@ -1,7 +1,7 @@
 /*eslint-env browser, jquery*/
 
 //players array
-var roomNum = sessionStorage.getItem('RoomNumber');
+var roomNum = 0;
 var players = [];
 
 function startGame() {
@@ -9,22 +9,32 @@ function startGame() {
 	//get all players
 	for(var x = 1; x < 11; x++) {
 		var playerName = document.getElementById("name" + x).value;
-		var playerNum = document.getElementById("number1" + x).value;
-		
+		var playerNum = document.getElementById("number" + x).value;
+		console.log(playerName);
 		players[x - 1] = playerName;
 	}
-	console.log(roomNum);
-	console.log(players);
-	
+
+	//get room number
+	roomNum = document.getElementById("roomNum").value;
+
+	//safety checks
+	if(roomNum < 0 || roomNum == "") {
+		return null;
+	}
 	//make sure enough players have been entered
 	if(players.length < 5 || players.length > 10) {
 		return null;
 	}
 	
+	console.log(roomNum);
+	console.log(players);
+	
 	//json to be posted
 	var json;
 	json.room = roomNum;
 	json.playerList = players;
+	
+	console.log(json);
 	
 	//post data to server
 	$.post( "/join", json);
