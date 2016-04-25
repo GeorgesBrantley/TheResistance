4e838a847a109ab55363bb6fea34f3c8246bdfe2
Gordon Read <gordonmread@gmail.com>
2016-04-25 08:18:57 +0000

    new method might work?

--- public/host.js
+++ public/host.js
@@ -16,27 +16,30 @@ window.onload = function getRoom() {
 	var roomNumThing = document.getElementById(roomNum);
 	roomNumThing.innerHTML = "Game: " + sessionStorage.roomNum;
 	
-	$.get("/" + sessionStorage.roomNum + "/getLeaderList", function( data ){
+	
+	var roundCircles = [];
+	roundCircles.add(document.getElementById('round1Cir'));
+	roundCircles.add(document.getElementById('round2Cir'));
+	roundCircles.add(document.getElementById('round3Cir'));
+	roundCircles.add(document.getElementById('round4Cir'));
+	roundCircles.add(document.getElementById('round5Cir'));
+	
+	var numberTexts = [];
+	numberTexts.add(document.getElementById('round1Num'));
+	numberTexts.add(document.getElementById('round2Num'));
+	numberTexts.add(document.getElementById('round3Num'));
+	numberTexts.add(document.getElementById('round4Num'));
+	numberTexts.add(document.getElementById('round5Num'));		
+	
+	while (sessionStorage.numPlayer === null) {
+		continue;
+	}
+	
+	var numPlayers = sessionStorage.numPlayers;
 		
-		var roundCircles = [];
-		roundCircles.add(document.getElementById('round1Cir'));
-		roundCircles.add(document.getElementById('round2Cir'));
-		roundCircles.add(document.getElementById('round3Cir'));
-		roundCircles.add(document.getElementById('round4Cir'));
-		roundCircles.add(document.getElementById('round5Cir'));
-		
-		var numberTexts = [];
-		numberTexts.add(document.getElementById('round1Num'));
-		numberTexts.add(document.getElementById('round2Num'));
-		numberTexts.add(document.getElementById('round3Num'));
-		numberTexts.add(document.getElementById('round4Num'));
-		numberTexts.add(document.getElementById('round5Num'));
-		
-		var numPlayers = data.length;
-		
-		for(i = 0; i < numberTexts.length; i++) {
-			numberTexts[i].innerHTML = roundNumbers[numPlayers-5][i];
-		}
+	for(i = 0; i < numberTexts.length; i++) {
+		numberTexts[i].innerHTML = roundNumbers[numPlayers-5][i];
+	};
 
 	});
 };
