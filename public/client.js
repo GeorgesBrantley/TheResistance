baab343f5eaef696508a757db1b49ce39b07d5f2
Matthew Clase <mclase@purdue.edu>
2016-04-25 00:39:12 +0000

    trying again

--- public/client.js
+++ public/client.js
@@ -6,19 +6,19 @@ function getList() {
 	
 	//denote current leader
 	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-	xmlhttp.open("GET", "/" + roomNum + "/getLeader");
+	xmlhttp.open("GET", "/" + roomNum + "/getLeader", true);
 	xmlhttp.send(null);
-	console.log("/getLeader: " + xmlhttp.responseText);
+	console.log("/getLeader: " + xmlhttp.responseText.name);
 	leader = xmlhttp.responseText.name;
 	var str = "Current leader is " + leader;
 	document.getElementById("leader").innerHTML = str;
 	
 	//create selectable list
-	xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
-	xmlhttp.open("GET", "/" + roomNum + "/getPlayers");
-	xmlhttp.send(null);
-	console.log("/getPlayers: " + xmlhttp.responseText);
-	var players = xmlhttp.responseText;
+	var xmlhttp2 = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp2.open("GET", "/" + roomNum + "/getPlayers");
+	xmlhttp2.send(null);
+	console.log("/getPlayers: " + xmlhttp2.responseText);
+	var players = xmlhttp2.responseText;
 	
 	console.log("Size of players: " + players.length);
 	for(var x = 0; x < players.length; x++) {
