72bfab232668f9b72c8e4efc18abc592b9cdac92
Matthew Clase <mclase@purdue.edu>
2016-04-25 05:12:48 +0000

    gfsdgfsd

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 /*eslint-env browser, jquery*/
 var roomNum = sessionStorage.getItem("roomNum");
-var players;
+var players = [];
 
 console.log(roomNum);
 function getList() {
@@ -28,19 +28,21 @@ function getList() {
 */
 
 
-	getPlayers();
-	var table = document.getElementById("playerList");
+	$.get( "/" + roomNum + "/getLeaderList", function( data ) {
+		var table = document.getElementById("playerList");
+		console.log("Size of players: " + data.length);
+		for(var x = 0; x < data.length; x++) {
+			console.log("data[x].name = " + data[x].name);
+			var row = table.insertRow(x);
+			var cell1 = row.insertCell(0);
+			cell1.innerHTML = players.name[x];
+		}
+		
+	});
 
 	
-	console.log("Size of players: " + players.length);
-	for(var x = 0; x < players.length; x++) {
-		console.log("data[x].name = " + players[x].name);
-		var row = table.insertRow(x);
-		var cell1 = row.insertCell(0);
-		cell1.innerHTML = players.name[x];
-	}
 }
-
+/*
 function getPlayers() {
 	$.get( "/" + roomNum + "/getLeaderList", function( data ) {
 		if(data === "" || data === null) {
@@ -50,3 +52,4 @@ function getPlayers() {
 		players = data;
 	});
 }
+*/
