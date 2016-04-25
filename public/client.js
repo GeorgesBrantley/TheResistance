909e58a88fdf5c3e32b86cd5d5f7d473a9421b16
Matthew Clase <mclase@purdue.edu>
2016-04-25 00:01:14 +0000

    tired of testing issues

--- public/client.js
+++ public/client.js
@@ -1,13 +1,35 @@
 /*eslint-env browser, jquery*/
 var roomNum = sessionStorage.getItem("roomNum");
+var leader;
 console.log(roomNum);
-function getLeader() {
-		console.log("/" + roomNum + "/getLeader");
-		$.get( "/" + roomNum + "/getLeader" , function( data ) {
-			var leader = data.name;
-			console.log("data: " + data);
-			console.log("leader: " + leader);
-			var str = "Current leader is " + leader;
-			document.getElementById("leader").innerHTML = str;
-		});
+function getList() {
+	
+	//denote current leader
+	var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp.open("GET", "/" + roomNum + "/getLeader");
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xmlhttp.send(null);
+	console.log("/getLeader: " + xmlhttp.responseText);
+	leader = xmlhttp.responseText.name;
+	var str = "Current leader is " + leader;
+	document.getElementById("leader").innerHTML = str;
+	
+	//create selectable list
+	xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+	xmlhttp.open("GET", "/" + roomNum + "/getPlayers");
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xmlhttp.send(null);
+	console.log("/getPlayers: " + xmlhttp.responseText);
+	var players = xmlhttp.responseText;
+	
+	console.log("Size of players: " + players.length);
+	for(var x = 0; x < players.length; x++) {
+		var option = document.createElement("option");
+		option.value = x;
+		option.innerHTML = players[x].name;
+		
+		document.getElementById("playerList").appendChild(option);
+		
+		
+	}
 }
