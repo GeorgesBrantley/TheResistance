731994ece3d002e0fda2503132f7eb375d331a9d
Gordon Read <gordonmread@gmail.com>
2016-04-25 07:26:24 +0000

    added start page redirect

---
+++ public/start.js
@@ -0,0 +1,27 @@
+
+/*eslint-env browser, jquery*/
+console.log("running js");
+var url = "resistancegame.mybluemix.net";
+var roomNum;
+
+function askServer() {
+	return $.get("/" + roomNum + "/getLeaderList").length;
+}
+
+//get room number
+window.onload = function getRoom() {
+
+	$.get( "/host", function( data ) {
+	document.getElementById("RoomNum").innerHTML = data;
+	roomNum = data;
+	});
+	
+	var dataLength = 0;
+	while (dataLength === 0) {
+		dataLength = setTimeout('askServer', 1000);
+	}
+	window.location.href = url + "/host.html";
+};
+
+
+//get initial game info
